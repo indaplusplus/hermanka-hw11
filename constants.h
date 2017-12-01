a2bef4c668af12fd7079b3eb6426885420b2cc0b
Herkarl <herkarlsson@gmail.com>
2017-12-01 01:37:53 +0100

    removed some lines

--- constants.h
+++ constants.h
@@ -2,21 +2,16 @@
 #define FILESYSTEM_CONSTANTS_H
 
 #include <vector>
-//#include <cstdint>
 #include <string>
 #include <cryptopp/config.h>
-//#include <cryptopp/dh2.h>
 
-//namespace constants {
 
-  //typedef uint8_t ByteType;
-  typedef std::string HashType;
-  typedef std::vector<byte> BlockType;
+typedef std::string HashType;
+typedef std::vector<byte> BlockType;
 
-  constexpr int BLOCK_SIZE = 1<<10;
-  constexpr int FILESYSTEM_SIZE = 1<<10; //Number of blocks
-  const BlockType emptyBlock = BlockType(BLOCK_SIZE);
+constexpr int BLOCK_SIZE = 1<<10;
+constexpr int FILESYSTEM_SIZE = 1<<10; //Number of blocks
+const BlockType emptyBlock = BlockType(BLOCK_SIZE);
 
-//}
 
 #endif //FILESYSTEM_CONSTANTS_H
